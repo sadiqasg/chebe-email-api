33d59ca2519e22bf5ca60200366f1aa1e395f80e
sadiqasg <sadiqasg@gmail.com>
2024-02-05 09:56:38 +0100

    added delete endpoint and restructured post email

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -20,26 +20,25 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-
 // Function to read waitlist from file
 const readWaitlistFromFile = () => {
-    const filePath = path.join(__dirname, 'waitlist.json');
+    const filePath = path.join(__dirname, "waitlist.json");
     try {
-        const data = fs.readFileSync(filePath, 'utf8');
+        const data = fs.readFileSync(filePath, "utf8");
         return JSON.parse(data);
     } catch (error) {
-        console.error('Error reading waitlist file:', error);
+        console.error("Error reading waitlist file:", error);
         return [];
     }
 };
 
 // Function to write waitlist to file
 const writeWaitlistToFile = (waitlist) => {
-    const filePath = path.join(__dirname, 'waitlist.json');
+    const filePath = path.join(__dirname, "waitlist.json");
     try {
         fs.writeFileSync(filePath, JSON.stringify(waitlist, null, 2));
     } catch (error) {
-        console.error('Error writing waitlist file:', error);
+        console.error("Error writing waitlist file:", error);
     }
 };
 
@@ -52,29 +51,56 @@ app.post("/waitlist", async (req, res) => {
 
     const mailOptions = {
         from: "Chebe.ng <chebengwaitlist@gmail.com>",
-        to: "chebengwaitlist@gmail.com",
+        to: "sadiqasg@gmail.com",
+        // to: "chebengwaitlist@gmail.com",
         subject: "New User Added to Waitlist",
         text: `A new user with email ${userEmail} has been added to the waitlist.`,
     };
 
     try {
         const info = await transporter.sendMail(mailOptions);
-        console.log("Email sent:", info.response);
-        
+        res.status(200).json(info.response);
+
         const waitlist = readWaitlistFromFile();
         waitlist.push({ email: userEmail });
         writeWaitlistToFile(waitlist);
 
         return info;
     } catch (error) {
-        console.error("Error sending email:", error);
-        throw error;
+        console.error("Error sending email:", error.message, error.stack);
+        res.status(400).json(error);
     }
 });
 
 app.get("/waitlist", (req, res) => {
     const waitlist = readWaitlistFromFile();
     res.json(waitlist);
+});
+
+app.delete("/waitlist/all", async (req, res) => {
+    try {
+        writeWaitlistToFile([]);
+        res.status(204).json({ message: "All emails cleared" });
+    } catch (error) {
+        console.error("Error deleting waitlist:", error);
+        res.status(500).json({ error: "Failed to delete waitlist" });
+    }
+});
+
+app.delete("/waitlist/:email", async (req, res) => {
+    const emailToDelete = req.params.email;
+
+    try {
+        const waitlist = readWaitlistFromFile();
+        const updatedWaitlist = waitlist.filter(
+            (user) => user.email !== emailToDelete
+        );
+        writeWaitlistToFile(updatedWaitlist);
+        res.status(200).json({ message: `Email ${emailToDelete} deleted` });
+    } catch (error) {
+        console.error(`Error deleting email ${emailToDelete}:`, error);
+        res.status(500).json({ error: "Failed to delete email" });
+    }
 });
 
 app.listen(port, () => {
