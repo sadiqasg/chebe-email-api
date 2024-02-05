ed98f9dc093bf772821139f48aebce7898ca7cbb
sadiqasg <sadiqasg@gmail.com>
2024-02-05 10:24:47 +0100

    refactored to use postgres

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-const fs = require("fs");
-const path = require("path");
+const { Pool } = require("pg");
+
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,35 +13,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set("trust proxy", true);
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
+pool.connect((err, client, done) => {
+    if (err) {
+        console.error("Error connecting to the database", err);
+    } else {
+        console.log("Connected to the database");
+    }
+});
+
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: "chebengwaitlist@gmail.com",
-        pass: "klpgpsogojexbsri",
+        user: process.env.AUTH_EMAIL,
+        pass: process.env.AUTH_PASS,
     },
 });
-
-// Function to read waitlist from file
-const readWaitlistFromFile = () => {
-    const filePath = path.join(__dirname, "waitlist.json");
-    try {
-        const data = fs.readFileSync(filePath, "utf8");
-        return JSON.parse(data);
-    } catch (error) {
-        console.error("Error reading waitlist file:", error);
-        return [];
-    }
-};
-
-// Function to write waitlist to file
-const writeWaitlistToFile = (waitlist) => {
-    const filePath = path.join(__dirname, "waitlist.json");
-    try {
-        fs.writeFileSync(filePath, JSON.stringify(waitlist, null, 2));
-    } catch (error) {
-        console.error("Error writing waitlist file:", error);
-    }
-};
 
 app.get("/", (req, res) => {
     res.status(200).send("Email API");
@@ -51,35 +43,35 @@ app.post("/waitlist", async (req, res) => {
 
     const mailOptions = {
         from: "Chebe.ng <chebengwaitlist@gmail.com>",
-        to: "sadiqasg@gmail.com",
-        // to: "chebengwaitlist@gmail.com",
+        to: process.env.MAIL_TO,
         subject: "New User Added to Waitlist",
         text: `A new user with email ${userEmail} has been added to the waitlist.`,
     };
 
     try {
         const info = await transporter.sendMail(mailOptions);
-        res.status(200).json(info.response);
-
-        const waitlist = readWaitlistFromFile();
-        waitlist.push({ email: userEmail });
-        writeWaitlistToFile(waitlist);
-
-        return info;
+        await pool.query("INSERT INTO waitlist (email) VALUES ($1)", [
+            userEmail,
+        ]);
+        res.status(201).json({
+            message: "Email added to waitlist",
+            response: info.response,
+        });
     } catch (error) {
         console.error("Error sending email:", error.message, error.stack);
         res.status(400).json(error);
     }
 });
 
-app.get("/waitlist", (req, res) => {
-    const waitlist = readWaitlistFromFile();
+app.get("/waitlist", async (req, res) => {
+    const result = await pool.query("SELECT email FROM waitlist");
+    const waitlist = result.rows.map((row) => ({ email: row.email }));
     res.json(waitlist);
 });
 
-app.delete("/waitlist/all", async (req, res) => {
+app.get("/waitlist/delete/all", async (req, res) => {
     try {
-        writeWaitlistToFile([]);
+        await pool.query("DELETE FROM waitlist");
         res.status(204).json({ message: "All emails cleared" });
     } catch (error) {
         console.error("Error deleting waitlist:", error);
@@ -87,15 +79,13 @@ app.delete("/waitlist/all", async (req, res) => {
     }
 });
 
-app.delete("/waitlist/:email", async (req, res) => {
+app.get("/waitlist/delete/:email", async (req, res) => {
     const emailToDelete = req.params.email;
 
     try {
-        const waitlist = readWaitlistFromFile();
-        const updatedWaitlist = waitlist.filter(
-            (user) => user.email !== emailToDelete
-        );
-        writeWaitlistToFile(updatedWaitlist);
+        await pool.query("DELETE FROM waitlist WHERE email = $1", [
+            emailToDelete,
+        ]);
         res.status(200).json({ message: `Email ${emailToDelete} deleted` });
     } catch (error) {
         console.error(`Error deleting email ${emailToDelete}:`, error);
